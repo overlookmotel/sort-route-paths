1bdf9cf74ba21ed74792446080f3205311801a9c
overlookmotel <theoverlookmotel@gmail.com>
2018-07-22 17:17:32 +0100

    Initial release

--- README.md
+++ README.md
@@ -13,6 +13,75 @@
 
 ## Usage
 
+Sort route paths so that static routes precede dynamic routes. i.e. `/foo/:id` ranked after `/foo/new`.
+
+This is the order you'd want to add the routes to [express](https://www.npmjs.com/package/express) so that static routes match first i.e. request for `/foo/new` is matched by `/foo/new` route, not `/foo/:id`.
+
+Ranking algorithm stolen from [sort-route-addresses](https://www.npmjs.com/package/sort-route-addresses) but this implementation also puts paths in alphabetical order.
+
+Returns a new array of the paths sorted. Does not alter original array.
+
+```js
+const sort = require('sort-route-paths');
+
+const paths = [
+  '/*',
+  '/:id',
+  '/foo',
+  '/foo/:id',
+  '/foo/bar',
+  '/'
+];
+
+console.log( sort(paths) );
+```
+
+Outputs:
+
+```js
+[
+  '/',
+  '/foo',
+  '/foo/bar',
+  '/foo/:id',
+  '/:id',
+  '/*'
+]
+```
+
+### Sorting objects
+
+Optional 2nd argument can be used to sort objects by a property containing the path.
+
+#### With function
+
+```js
+const routes = [
+  {id: 1, path: '/foo/:id'},
+  {id: 2, path: '/foo/new'}
+];
+
+const sortedRoutes = sort( routes, route => route.path );
+```
+
+Results in:
+
+```js
+[
+  {id: 2, path: '/foo/new'},
+  {id: 1, path: '/foo/:id'}
+];
+```
+
+#### With string
+
+Or just provide property name as a string. These two are equivalent:
+
+```js
+sort( routes, route => route.path );
+sort( routes, 'path' );
+```
+
 ## Tests
 
 Use `npm test` to run the tests. Use `npm run cover` to check coverage.
--- test/all.test.js
+++ test/all.test.js
@@ -16,10 +16,121 @@ chai.config.includeStack = true;
 // Tests
 
 /* jshint expr: true */
-/* global describe, it */
+/* global describe, it, beforeEach */
 
-describe('Tests', function() {
-	it.skip('all', function() {
-		expect(sort).to.be.ok;
+const paths = [
+	'/',
+	'/login',
+	'/artists',
+	'/artists/new',
+	'/artists/*',
+	'/artists/:id',
+	'/artists/:id/edit',
+	'/artists/:id/delete',
+	'/artists/albums',
+	'/artists/albums/new',
+	'/artists/albums/:id',
+	'/artists/albums/:id/edit',
+	'/artists/albums/:id/delete',
+	'/artists/:id/albums',
+	'/artists/:id/albums/new',
+	'/artists/:id/albums/:id',
+	'/artists/:id/albums/:id/edit',
+	'/artists/:id/albums/:id/delete',
+	'/*',
+	'/:foo/:bar/:baz',
+	'/*/baz/*'
+];
+
+const expectedOutput = [
+	'/',
+	'/artists',
+	'/artists/albums',
+	'/artists/albums/new',
+	'/artists/albums/:id',
+	'/artists/albums/:id/delete',
+	'/artists/albums/:id/edit',
+	'/artists/new',
+	'/artists/:id',
+	'/artists/:id/albums',
+	'/artists/:id/albums/new',
+	'/artists/:id/albums/:id',
+	'/artists/:id/albums/:id/delete',
+	'/artists/:id/albums/:id/edit',
+	'/artists/:id/delete',
+	'/artists/:id/edit',
+	'/artists/*',
+	'/login',
+	'/*/baz/*',
+	'/:foo/:bar/:baz',
+	'/*'
+];
+
+describe('`sort()`', function() {
+	beforeEach(function() {
+		this.input = paths.concat();
+		this.output = sort(this.input);
+	});
+
+	it('returns array', function() {
+		expect(this.output).to.be.an('array');
+	});
+
+	it('returns new array', function() {
+		expect(this.output).not.to.equal(this.input);
+	});
+
+	it('returns sorted', function() {
+		expect(this.output).to.deep.equal(expectedOutput);
+	});
+
+	it('does not alter input', function() {
+		expect(this.input).to.deep.equal(paths);
+	});
+});
+
+describe('`sort()` with accessor function', function() {
+	beforeEach(function() {
+		this.input = paths.map(path => ({path}));
+		this.output = sort(this.input, obj => obj.path);
+	});
+
+	it('returns array', function() {
+		expect(this.output).to.be.an('array');
+	});
+
+	it('returns new array', function() {
+		expect(this.output).not.to.equal(this.input);
+	});
+
+	it('returns sorted', function() {
+		expect(this.output).to.deep.equal(expectedOutput.map(path => ({path})));
+	});
+
+	it('does not alter input', function() {
+		expect(this.input).to.deep.equal(paths.map(path => ({path})));
+	});
+});
+
+describe('`sort()` with accessor string', function() {
+	beforeEach(function() {
+		this.input = paths.map(path => ({path}));
+		this.output = sort(this.input, 'path');
+	});
+
+	it('returns array', function() {
+		expect(this.output).to.be.an('array');
+	});
+
+	it('returns new array', function() {
+		expect(this.output).not.to.equal(this.input);
+	});
+
+	it('returns sorted', function() {
+		expect(this.output).to.deep.equal(expectedOutput.map(path => ({path})));
+	});
+
+	it('does not alter input', function() {
+		expect(this.input).to.deep.equal(paths.map(path => ({path})));
 	});
 });
